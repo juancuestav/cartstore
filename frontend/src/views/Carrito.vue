a92febbad3a14016fc80202866a6ff502674fcfb
alvarorvn <alvaro.rvn97@gmail.com>
2020-12-04 01:09:17 -0500

    array carrito de compra

--- frontend/src/views/Carrito.vue
+++ frontend/src/views/Carrito.vue
@@ -2,55 +2,63 @@
   <v-container>
     <v-row>
       <v-col md="8">
-
         <v-card>
-          <v-data-table :headers="headers" :items="carrito_list" hide-default-footer >
-
+          <v-data-table
+            :headers="headers"
+            :items="carrito_list"
+            hide-default-footer
+          >
             <template v-slot:item.prod_image="{ item }">
               <div class="pa-2 d-flex flex-column align-center">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.png" height="120px" width="150px"></v-img>
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                  height="120px"
+                  width="150px"
+                ></v-img>
               </div>
             </template>
 
             <template v-slot:item.actions="{ item }">
               <v-icon color="red"> mdi-delete </v-icon>
             </template>
-
           </v-data-table>
-
         </v-card>
-
       </v-col>
       <v-col md="4">
         <v-card>
-
-          <v-card-title class="blue-grey darken-1 white--text">Generar Compra</v-card-title>
+          <v-card-title class="blue-grey darken-1 white--text"
+            >Generar Compra</v-card-title
+          >
           <v-divider></v-divider>
-          
+
           <v-card-text>
             <v-container>
-
               <v-row>
                 <v-col cols="12">
                   <v-toolbar-title>Subtotal:</v-toolbar-title>
                 </v-col>
                 <v-col cols="12">
-                  <v-toolbar-title class="blue-grey lighten-5">{{sub_total}}</v-toolbar-title>
+                  <v-toolbar-title class="blue-grey lighten-5">{{
+                    sub_total
+                  }}</v-toolbar-title>
                 </v-col>
                 <v-col cols="12">
                   <v-toolbar-title>IVA 12%:</v-toolbar-title>
                 </v-col>
                 <v-col cols="12">
-                  <v-toolbar-title class="blue-grey lighten-5">{{iva}}</v-toolbar-title>
+                  <v-toolbar-title class="blue-grey lighten-5">{{
+                    iva
+                  }}</v-toolbar-title>
                 </v-col>
                 <v-col cols="12">
                   <v-toolbar-title>Total a Pagar:</v-toolbar-title>
                 </v-col>
                 <v-col cols="12">
-                  <v-toolbar-title class="blue-grey lighten-5">{{total_pagar}}</v-toolbar-title>
+                  <v-toolbar-title class="blue-grey lighten-5">{{
+                    total_pagar
+                  }}</v-toolbar-title>
                 </v-col>
               </v-row>
-
             </v-container>
           </v-card-text>
 
@@ -60,12 +68,17 @@
             <v-container>
               <v-row>
                 <v-col cols="12" class="py-0">
-                  <v-btn :to="{ name: 'Envio' }" color="blue" class="ma-2 white--text" block >Realizar Pago</v-btn>
+                  <v-btn
+                    :to="{ name: 'Envio' }"
+                    color="blue"
+                    class="ma-2 white--text"
+                    block
+                    >Realizar Pago</v-btn
+                  >
                 </v-col>
               </v-row>
             </v-container>
           </v-card-actions>
-
         </v-card>
       </v-col>
     </v-row>
@@ -77,7 +90,6 @@ import axios from "axios";
 export default {
   data() {
     return {
-      carrito_indices: [101, 102],
       sub_total: 0,
       iva: 0,
       total_pagar: 0,
@@ -90,48 +102,84 @@ export default {
         foto: null,
       },
       headers: [
-        {text: "Nombre", value: "nombre", sortable: false, class: "blue-grey accent-3 white--text", align: "center",},
-        {text: "Imagen", value: "foto", sortable: false, class: "blue-grey accent-3 white--text", align: "center",},
-        {text: "Precio", value: "precio", sortable: false, class: "blue-grey accent-3 white--text", align: "center",},
-        {text: "Stock", value: "stock", sortable: false, class: "blue-grey accent-3 white--text", align: "center",},
-        {text: "Acciones", value: "actions", sortable: false, class: "blue-grey accent-3 white--text", align: "center",},
+        {
+          text: "Nombre",
+          value: "nombre",
+          sortable: false,
+          class: "blue-grey accent-3 white--text",
+          align: "center",
+        },
+        {
+          text: "Imagen",
+          value: "foto",
+          sortable: false,
+          class: "blue-grey accent-3 white--text",
+          align: "center",
+        },
+        {
+          text: "Precio",
+          value: "precio",
+          sortable: false,
+          class: "blue-grey accent-3 white--text",
+          align: "center",
+        },
+        {
+          text: "Stock",
+          value: "stock",
+          sortable: false,
+          class: "blue-grey accent-3 white--text",
+          align: "center",
+        },
+        {
+          text: "Acciones",
+          value: "actions",
+          sortable: false,
+          class: "blue-grey accent-3 white--text",
+          align: "center",
+        },
       ],
     };
   },
-  created: function() {
-    
+  computed: {
+    carrito() {
+      return this.$store.state.indiceCarrito;
+    },
+  },
+  created: function () {
     //console.log("Este es el sub total CR: " + this.sub_total)
     //console.log("Carrito lleno")
     //console.log(this.carrito_list)
   },
-  mounted: function(){
-    console.log("Dentro de created")
-    this.carrito_indices.forEach(indice => {
-      axios.get("http://localhost:8080/productos/getproductscarrito/" + indice).then((response) => {
-        //console.log(response.data);
-        //this.producto = Object.assign({}, response.data)
-        var parsedobj = JSON.parse(JSON.stringify(response.data))
-        console.log(parsedobj)
-        this.carrito_list.push(parsedobj);
-        //console.log("Este es el precio: " + response.data.precio)
-        //console.log("Este es el sub total: " + this.sub_total)
-      });
-    })
+  mounted: function () {
+    console.log("Dentro de created");
+    this.carrito.forEach((indice) => {
+      axios
+        .get("http://localhost:8080/productos/getproductscarrito/" + indice)
+        .then((response) => {
+          console.log(response.data);
+          //this.producto = Object.assign({}, response.data)
+          /*var parsedobj = JSON.parse(JSON.stringify(response.data));
+          console.log(parsedobj);
+          this.carrito_list.push(parsedobj);*/
+          //console.log("Este es el precio: " + response.data.precio)
+          //console.log("Este es el sub total: " + this.sub_total)
+        });
+    });
     //this.$nextTick(() => {
-    
-      console.log("Dentro de mounted")
-      console.log(this.carrito_list)
-      console.log(this.carrito_list[0])
-    this.carrito_list.forEach(item => {
-      console.log("Dentro de for each")
+
+    /*console.log("Dentro de mounted");
+    console.log(this.carrito_list);
+    console.log(this.carrito_list[0]);
+    this.carrito_list.forEach((item) => {
+      console.log("Dentro de for each");
       this.sub_total += item.precio;
-      console.log("Este es el sub total MOU: " + this.sub_total)
+      console.log("Este es el sub total MOU: " + this.sub_total);
       this.iva = this.sub_total * 0.12;
       //this.iva = 35;
-      console.log("Este es el IVA MOU: " + this.iva)
+      console.log("Este es el IVA MOU: " + this.iva);
       this.total_pagar += this.iva + this.sub_total;
-    });
+    });*/
     //})
-  }
+  },
 };
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,32 +2,36 @@
   <v-container>
     <v-layout wrap>
       <v-flex v-for="producto in lista_productos" :key="producto.id">
-
         <v-card class="mx-auto" max-width="340" style="margin: 10px">
-
           <v-card-title>
-            <v-row class="mx-1">
-              <v-icon class="black--text" medium left >mdi-store</v-icon>
-              <span class="title black--text">{{producto.nombre}}</span>
+            <v-row style="margin: 0px 2px">
+              <v-icon class="black--text" medium left style="margin-right: 0px"
+                >mdi-store</v-icon
+              >
+              <h5 class="black--text">{{ producto.nombre }}</h5>
               <v-spacer></v-spacer>
-              <v-icon class="black--text" medium left >mdi-arrow-up-bold-box-outline</v-icon>
-              <span class="title">{{producto.stock}}</span>
+              <v-icon class="black--text" medium left style="margin-right: 0px"
+                >mdi-arrow-up-bold-box-outline</v-icon
+              >
+              <h5>{{ producto.stock }}</h5>
             </v-row>
           </v-card-title>
 
           <v-card-text>
-            <v-img contain height="130" :src=producto.foto></v-img>
+            <v-img contain height="130" :src="producto.foto"></v-img>
             <div class="mt-6 mx-1 text-justify">
               {{ producto.descripcion }}
             </div>
           </v-card-text>
 
-          <v-card-actions class="justify-center">
-            <v-btn color="blue" outlined > 
-              Agregar al carrito <v-icon medium right >mdi-cart</v-icon>
-            </v-btn>
-            <v-btn color="error" depressed> Comprar </v-btn>
-          </v-card-actions>
+          <div v-if="currentDataUser.us_rol != 'ADMINISTRADOR'">
+            <v-card-actions class="justify-center">
+              <v-btn small color="blue" outlined @click="add_cart(producto.id)">
+                Agregar al carrito <v-icon medium right>mdi-cart</v-icon>
+              </v-btn>
+              <v-btn small color="error" depressed> Comprar </v-btn>
+            </v-card-actions>
+          </div>
         </v-card>
       </v-flex>
     </v-layout>
@@ -45,15 +49,28 @@ export default {
       lista_productos: [],
     };
   },
-  created: function() {
+  computed: {
+    currentDataUser() {
+      return this.$store.state.datosUsuario;
+    },
+    carrito() {
+      return this.$store.state.indiceCarrito;
+    },
+  },
+  methods: {
+    add_cart(id_prod) {
+      if (!this.carrito.includes(id_prod)) {
+        this.carrito.push(id_prod);
+      }
+    },
+  },
+  created: function () {
     axios.get("http://localhost:8080/productos/listar").then((response) => {
       this.lista_productos = response.data.filter((p) => p != null);
-      console.log(response.data);
       this.lista_productos.forEach((elem) => {
         elem.foto = this.servidor + elem.foto;
-        console.log(response.data);
       });
     });
-  }
+  },
 };
 </script>
