b87f231c3dd90a67ca7a3f64ebbd639dbd14a54a
alvarorvn <alvaro.rvn97@gmail.com>
2020-12-03 21:04:53 -0500

    Falta editar productos

--- frontend/src/services/products.service.js
+++ frontend/src/services/products.service.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'http://localhost:8080';
 
 class ProductService {
     add_product(formData) {
-        return axios.post(`${API_URL}/product`, formData).then(function () {
-            console.log('SUCCESS!!');
-        }).catch(function () {
-            console.log('FAILURE!!');
-        });
+        return axios.post(`${API_URL}/productos/guardar`, formData);
+    }
+
+    get_products() {
+        return axios.get(`${API_URL}/productos/listar`);
+    }
+
+    delete_products(prod_id) {
+        return axios.delete(`${API_URL}/productos/eliminar/${prod_id}`);
     }
 }
+
+export default new ProductService();
